16773abd454aadb370b289d71782a77e81c729b2
Bairdwolf <136029428+Bairdwolf@users.noreply.github.com>
2025-04-27 11:06:32 -0500

    Add first three methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,21 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    count=-1
+    self.each do|elem|
+      count+=1
+      yield(elem, count)
+    end
+    return self
+  end
+
+  def my_select(&my_block)
+    output=[]
+    self.each do |elem|
+      output.push(elem) if my_block.call(elem)==true
+    end
+    return output
+  end
+
 end
 
 # You will first have to define my_each
@@ -7,5 +23,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    self.each do |elem|
+      yield(elem)
+    end
+    return self
+  end
 end
